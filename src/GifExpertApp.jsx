ef6bfd21e34729af6b05b390a63a6afbe1414b37
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-03 12:10:06 +0200

    input component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react"
 
 export const GifExpertApp = ()=>{
-    const [categories, setCategories] = useState(['One Punch','Ramonet','ChiquitoDeLaCalzada'])
+    const [categories, setCategories] = useState(['One Punch','Ramonet','ChiquitoDeLaCalzada']);
+    const onAddCateggory = ()=>{
+        const hero = 'luthien';
+        setCategories((prev) =>[...prev, hero] )
+    }
     return (
         <>
             <h1>GifExpertApp</h1>
+            
             <ol>
                 {categories.map((category)=>{
                     return <li key={category}>{category}</li>;
                 })} 
                 
             </ol>
+            <button onClick={onAddCateggory}>Add</button>
         </>
     )
 }
