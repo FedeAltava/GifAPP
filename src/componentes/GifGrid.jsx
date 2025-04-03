6fd3db2b5fde01a7dad5ceb0566462e7f589e89e
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-03 14:15:19 +0200

    chore created new file for fecth api

--- src/componentes/GifGrid.jsx
+++ src/componentes/GifGrid.jsx
@@ -1,23 +1,10 @@
+import { getGifs } from "../helpers/getGifs";
 
-export const GifGrid = ( { category } )=>{
-    const getGifts = async()=>{
-        const url = `http://api.giphy.com/v1/gifs/search?api_key=lbtaS4TeDXv0Y6PkuV7tiWIPaXnQa7H3&q=${category}&limit=20`;
-        const resp = await fetch(url);
-        const { data } = await resp.json();
-
-        const gifs = data.map(img=>({
-            id: img.id,
-            title: img.title,
-            url : img.images.downsized_medium.url
-        }));
-
-        console.log(gifs)
-    }
-    getGifts();
-    return (
-
-        <>
-            <h3>{ category }</h3>
-        </>
-    )
-}
+export const GifGrid = ({ category }) => {
+  getGifs(category);
+  return (
+    <>
+      <h3>{category}</h3>
+    </>
+  );
+};
