4d00831da7268b7eb6b86e96191ae48cc86517cd
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-03 11:51:57 +0200

    list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 export const GifExpertApp = ()=>{
+    const [categories, setCategories] = useState(['One Punch','Ramonet','ChiquitoDeLaCalzada'])
     return (
         <>
             <h1>GifExpertApp</h1>
+            <ol>
+                {categories.map((category)=>{
+                    return <li key={category}>{category}</li>;
+                })} 
+                
+            </ol>
         </>
     )
 }
