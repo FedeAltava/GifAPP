e45aeee4f036d7a7ae0e603fd99018b8b90eccbd
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-10 09:25:05 +0200

    the way how the  app get gifs is changed from http to https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
   try {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=lbtaS4TeDXv0Y6PkuV7tiWIPaXnQa7H3&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=lbtaS4TeDXv0Y6PkuV7tiWIPaXnQa7H3&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map((img) => ({
