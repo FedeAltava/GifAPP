376fbcb8c83ccf3243f79008a043fa19fa012b95
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-15 10:08:20 +0200

    third test AddCategory component

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -17,12 +17,21 @@ describe('AddCategory Component Tests', ()=>{
         expect( input.value).toBe('Saitama');
     });
 
-    it('should call onNewCategory if vale input exists',()=>{
+    it('should call onNewCategory if value input exists',()=>{
         render(<AddCategory onNewCategory={onNewCategory} />);
          const input = screen.getByRole('textbox');
          const form = screen.getByRole('form');
          fireEvent.input( input, { target: { value : inputValue } } );
          fireEvent.submit( form );
          expect(input.value).toBe('')
+         expect( onNewCategory ).toHaveBeenCalledTimes(1);
     });
+
+    it('should not call onNewCategory if value is empty',()=>{
+        render(<AddCategory onNewCategory={onNewCategory} />);
+        const form = screen.getByRole('form');
+         fireEvent.submit( form );
+         expect( onNewCategory ).not.toHaveBeenCalled();
+    });
+    
 })
