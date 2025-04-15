a6e2a8b76e31ad42c71f51814ef2983fafcae340
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-15 09:45:07 +0200

    second test AddCategory component

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -5,11 +5,24 @@ import { AddCategory } from "../../src/componentes/AddCategory"
 
 
 describe('AddCategory Component Tests', ()=>{
+
     const onNewCategory = jest.fn();
+    const inputValue = 'Saitama';
+    beforeEach(() => jest.clearAllMocks());
+
     it('should change the value of the text box',()=>{
         render(<AddCategory onNewCategory={onNewCategory} />);
         const input = screen.getByRole('textbox');
         fireEvent.input(input, { target: { value: 'Saitama'}});
         expect( input.value).toBe('Saitama');
-    })
+    });
+
+    it('should call onNewCategory if vale input exists',()=>{
+        render(<AddCategory onNewCategory={onNewCategory} />);
+         const input = screen.getByRole('textbox');
+         const form = screen.getByRole('form');
+         fireEvent.input( input, { target: { value : inputValue } } );
+         fireEvent.submit( form );
+         expect(input.value).toBe('')
+    });
 })
